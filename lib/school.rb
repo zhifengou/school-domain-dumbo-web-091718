1ffb863229d3f8db3055f9e17b5334f6375145ed
zhifeng ou <zhifengou@hotmail.com>
2018-09-26 13:22:43 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,9 @@
 # code here!
 class School
+  attr_accessor:roster
 def initialize(name)
     @name = name
     @roster = {}
-  end
-  
-   def roster
-    @roster
   end
   
    def add_student(name, grade)
